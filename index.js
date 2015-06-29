9b7ad116ab290395ef96832e1e814d1a99516dfa
russEco <russstreet@economist.com>
2015-06-29 09:57:46 +0100

    added css

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import TabView from './../component-tabview';
+import ImageCaption from './../component-imagecaption';
 
 export default class ArticleTemplate extends React.Component {
 
@@ -14,7 +16,6 @@ export default class ArticleTemplate extends React.Component {
       src: "http://lorempixel.com/g/1190/669/people"
     }
   };
-
 
 
 
@@ -39,17 +40,117 @@ export default class ArticleTemplate extends React.Component {
       <div className="section group">
         <div className="span_10 article-body margin_1">{this.props.children}</div>
       </div>
+      <TabView>
+        <div title="Politics">
+    <div className="TabView--Views--Tint"></div>
+     <a href="#">
+       <figure className="TabView--View--Content">
+         <img src="http://lorempixel.com/g/300/169/cats"/>
+         <figcaption>this is the caption</figcaption>
+       </figure>
+     </a>
+     <a href="#">
+      <figure className="TabView--View--Content">
+        <img src="http://lorempixel.com/g/300/169/people"/>
+        <figcaption>this is the caption</figcaption>
+      </figure>
+     </a>
+     <a href="#">
+      <figure className="TabView--View--Content">
+        <img src="http://lorempixel.com/g/300/169/city"/>
+        <figcaption>this is the caption</figcaption>
+      </figure>
+     </a>
+     <a href="#">
+      <figure className="TabView--View--Content">
+        <img src="http://lorempixel.com/g/300/169/fashion"/>
+          <figcaption>this is the caption</figcaption>
+        </figure>
+     </a>
+    </div>
+    <div title="Business & Economics">
+    <div className="TabView--Views--Tint"></div>
+     <a href="#">
+       <figure className="TabView--View--Content">
+         <img src="http://lorempixel.com/g/300/169/food"/>
+         <figcaption>this is the caption</figcaption>
+       </figure>
+     </a>
+     <a href="#">
+      <figure className="TabView--View--Content">
+        <img src="http://lorempixel.com/g/300/169/nightlife"/>
+        <figcaption>this is the caption</figcaption>
+      </figure>
+     </a>
+     <a href="#">
+      <figure className="TabView--View--Content">
+        <img src="http://lorempixel.com/g/300/169/sports"/>
+        <figcaption>this is the caption</figcaption>
+      </figure>
+     </a>
+     <a href="#">
+      <figure className="TabView--View--Content">
+        <img src="http://lorempixel.com/g/300/169/nature"/>
+        <figcaption>this is the caption</figcaption>
+      </figure>
+     </a>
+    </div>
+    <div title="Science & technology">
+    <div className="TabView--Views--Tint"></div>
+     <a href="#">
+      <figure className="TabView--View--Content">
+        <img src="http://lorempixel.com/g/300/169/transport"/>
+        <figcaption>this is the caption</figcaption>
+      </figure>
+     </a>
+     <a href="#">
+      <figure className="TabView--View--Content">
+        <img src="http://lorempixel.com/g/300/169/abstract"/>
+        <figcaption>this is the caption</figcaption>
+      </figure>
+     </a>
+     <a href="#">
+      <figure className="TabView--View--Content">
+        <img src="http://lorempixel.com/g/300/169/technics"/>
+        <figcaption>this is the caption</figcaption>
+      </figure>
+     </a>
+     <a href="#">
+      <figure className="TabView--View--Content">
+        <img src="http://lorempixel.com/g/300/169/cats"/>
+        <figcaption>this is the caption</figcaption>
+      </figure>
+     </a>
+    </div>
+    <div title="History">
+    <div className="TabView--Views--Tint"></div>
+     <a href="#">
+      <figure className="TabView--View--Content">
+        <img src="http://lorempixel.com/g/300/169/nightlife"/>
+        <figcaption>this is the caption</figcaption>
+      </figure>
+     </a>
+     <a href="#">
+      <figure className="TabView--View--Content">
+        <img src="http://lorempixel.com/g/300/169/sports"/>
+        <figcaption>this is the caption</figcaption>
+      </figure>
+     </a>
+     <a href="#">
+      <figure className="TabView--View--Content">
+        <img src="http://lorempixel.com/g/300/169/food"/>
+        <figcaption>this is the caption</figcaption>
+      </figure>
+     </a>
+     <a href="#">
+      <figure className="TabView--View--Content">
+        <img src="http://lorempixel.com/g/300/169/city"/>
+        <figcaption>this is the caption</figcaption>
+      </figure>
+     </a>
+    </div>
+      </TabView>
     </div>
     );
-  }
-
-
-    _handleClick(selectedIndex, event) {
-      if(selectedIndex >= this.props.children.length){
-        selectedIndex = 0;
-      }
-     this.setState({
-      selectedIndex
-      });
   }
 }
